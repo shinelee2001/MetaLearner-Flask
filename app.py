ab2b4c3e18540f0d108460e5ecf5e66a07159ea1
Dongchan Lee <shinelee2001@gmail.com>
2024-02-10 21:18:57 -0500

    error handler update

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from main_routes import main_bp
 from fileConv_routes import fileConvert_bp
+from error_routes import error_bp
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 app.register_blueprint(main_bp)
 app.register_blueprint(fileConvert_bp)
+app.register_blueprint(error_bp)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
---
+++ error_routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+error_bp = Blueprint("error", __name__, template_folder="templates/errorTemplates")
+
+@error_bp.app_errorhandler(404)
+def page_note_found(error):
+    return render_template('404.html'), 404
